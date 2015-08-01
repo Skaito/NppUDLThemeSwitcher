2e7d9ecd3f13befa879eb9025dd3c054a6bbb616
Marius Tomas <marius@realmdev.lt>
2015-08-01 20:52:50 +0300

    Remove unused imports

--- src/main/java/lt/realmdev/nppudlthemeswitcher/UDLItem.java
+++ src/main/java/lt/realmdev/nppudlthemeswitcher/UDLItem.java
@@ -7,11 +7,9 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -20,8 +18,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
